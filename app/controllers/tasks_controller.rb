60a3118462511324f59c1764bbcdf830adeb4baa
Yeon Kim <awyeon@gmail.com>
2021-11-23 16:06:20 +0800

    modified views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,28 +2,47 @@
 
 # app/controller/tasks_controller.rb
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
-  def new
-    @tasks = Task.new(task_params)
-  end
+  def show; end
 
-  def show
-    @task = Task.find(params[:id])
+  def new
+    @task = Task.new(task_params)
   end
 
   def create
-    @task = Task.find(params[:id])
+    @task = Task.new(task_params)
     @task.save
 
     redirect_to task_path(@task)
+  end
+
+  def edit; end
+
+  def update
+    @task.update(params[:task])
+
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show'
+  # root to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create' # no view
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update' # no view
+
+  delete 'tasks/:id', to: 'tasks#destroy' # no view
 end
